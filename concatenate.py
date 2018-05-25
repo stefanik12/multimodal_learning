73cfe2021a01978690e2732661fd92fa8f85acd5
Michal Červeňanský <xcerven1@fi.muni.cz>
2018-05-25 18:23:25 +0200

    Some try to run save_vec_result which saves Series to pickle

--- concatenate.py
+++ concatenate.py
@@ -12,13 +12,10 @@ def concatenate(vector1, vector2):
     return np.concatenate((a, b))
 
 
-# In[2]:
+# In[4]:
 
 
-import doc2vec_wrapped
-import word2vec_wrapped
 import pandas as pd
-import inception_for_each_file
 
 
 def concat_ser_dic(series1, series2):
@@ -33,15 +30,15 @@ def concat_ser_dic(series1, series2):
 
 ## doc2vec+word2vec ##
 def doc2vec_word2vec():
-    return concat_ser_dic(doc2vec_wrapped.vectorize_content(vector_len=400), word2vec_wrapped.vectorize_content())
+    return concat_ser_dic(pd.read_pickle("./doc2vec"), pd.read_pickle("./word2vec"))
 
 def doc2vec_inception():
-    return concat_ser_dic(doc2vec_wrapped.vectorize_content(vector_len=400), inception_for_each_file("./data/Pascal_VOC_images"))
+    return concat_ser_dic(pd.read_pickle("./doc2vec"), pd.read_pickle("./inception"))
     
 def word2vec_inception():
-    return concat_ser_dic(word2vec_wrapped.vectorize_content(), inception_for_each_file("./data/Pascal_VOC_images"))
+    return concat_ser_dic(pd.read_pickle("./word2vec"), pd.read_pickle("./inception"))
 
 def doc2vec_word2vec_inception():
-    tmp = concat_ser_dic(doc2vec_wrapped.vectorize_content(vector_len=400), word2vec_wrapped.vectorize_content())
-    return concat_ser_dic(tmp, inception_for_each_file("./data/Pascal_VOC_images"))
+    tmp = concat_ser_dic(pd.read_pickle("./doc2vec"), pd.read_pickle("./word2vec"))
+    return concat_ser_dic(tmp, pd.read_pickle("./inception"))
 
--- save_vec_result.py
+++ save_vec_result.py
@@ -4,6 +4,7 @@
 # In[1]:
 
 
+# Do not use !
 def save_vec_result(series, outname):
     output = open(outname, "w")
     for word in series.index:
@@ -20,10 +21,18 @@ import word2vec_wrapped
 import pandas as pd
 import inception_for_each_file
 
-save_vec_result(doc2vec_wrapped.vectorize_content(vector_len=400), "doc2vec")
-print("doc2vec done")
-save_vec_result(word2vec_wrapped.vectorize_content(), "word2vec")
-print("word2vec done")
-save_vec_result(for_each_file("./data/Pascal_VOC_images"), "inception")
-print("inception done")
+def run_to_pickle():
+    doc2vec_wrapped.vectorize_content(vector_len=400).to_pickle("./doc2vec")
+    print("doc2vec done")
+    word2vec_wrapped.vectorize_content().to_pickle("./word2vec")
+    print("word2vec done")
+    for_each_file("./data/Pascal_VOC_images").to_pickle("./inception")
+    print("inception done")
+    return
+
+
+# In[ ]:
+
+
+run_to_pickle()
 
