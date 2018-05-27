a469803a2673df0bf81dba98c7fdce98cb3b1900
Michal Červeňanský <xcerven1@fi.muni.cz>
2018-05-27 11:49:42 +0200

    2500,2500 settings

--- MLPClassifier.py
+++ MLPClassifier.py
@@ -46,6 +46,36 @@ def cross_val_accuracy(classifier, df):
 
 # # doc2vec_inception_class
 
+# In[5]:
+
+
+def doc2vec_inception_class():
+    df = pd.read_json("data/COCO/coco-easier.txt", 
+                  lines=True)
+    df = df.sample(frac=1).reset_index(drop=True)
+    doc2vec_vectors_series = doc2vec_wrapped.vectorize_content()
+    df["concat_vectors"] = df.apply(lambda row: concatenate(doc2vec_vectors_series[row['file_name']],
+                                                           inception_df['vector'][row['file_name']]), axis=1)
+    X = df["concat_vectors"]
+    X
+
+    from sklearn.neural_network import MLPClassifier
+    X = df["concat_vectors"].tolist()
+    y = df["category"].tolist()
+    clf = MLPClassifier(solver='lbfgs', alpha=1e-5,
+                        hidden_layer_sizes=(2500, 2500), random_state=1)
+    clf.fit(X, y)                         
+    MLPClassifier(activation='relu', alpha=1e-05, batch_size='auto',
+           beta_1=0.9, beta_2=0.999, early_stopping=False,
+           epsilon=1e-08, hidden_layer_sizes=(5, 2), learning_rate='constant',
+           learning_rate_init=0.001, max_iter=200, momentum=0.9,
+           nesterovs_momentum=True, power_t=0.5, random_state=1, shuffle=True,
+           solver='lbfgs', tol=0.0001, validation_fraction=0.1, verbose=False,
+           warm_start=False)
+    print("doc2vec_inception")
+    cross_val_accuracy(clf, df)
+
+
 # # word2vec_inception_class
 
 # In[6]:
@@ -65,7 +95,7 @@ def word2vec_inception_class():
     X = df["concat_vectors"].tolist()
     y = df["category"].tolist()
     clf = MLPClassifier(solver='lbfgs', alpha=1e-5,
-                        hidden_layer_sizes=(20, 20), random_state=1)
+                        hidden_layer_sizes=(2500, 2500), random_state=1)
     clf.fit(X, y)                         
     MLPClassifier(activation='relu', alpha=1e-05, batch_size='auto',
            beta_1=0.9, beta_2=0.999, early_stopping=False,
@@ -98,7 +128,7 @@ def word2vec_doc2vec_class():
     X = df["concat_vectors"].tolist()
     y = df["category"].tolist()
     clf = MLPClassifier(solver='lbfgs', alpha=1e-5,
-                        hidden_layer_sizes=(5, 4), random_state=1)
+                        hidden_layer_sizes=(2500, 2500), random_state=1)
     clf.fit(X, y)                         
     MLPClassifier(activation='relu', alpha=1e-05, batch_size='auto',
            beta_1=0.9, beta_2=0.999, early_stopping=False,
@@ -134,7 +164,7 @@ def word2vec_doc2vec_inception_class():
     X = df["concat_vectors"].tolist()
     y = df["category"].tolist()
     clf = MLPClassifier(solver='lbfgs', alpha=1e-5,
-                        hidden_layer_sizes=(20, 20), random_state=1)
+                        hidden_layer_sizes=(2500, 2500), random_state=1)
     clf.fit(X, y)                         
     MLPClassifier(activation='relu', alpha=1e-05, batch_size='auto',
            beta_1=0.9, beta_2=0.999, early_stopping=False,
@@ -147,7 +177,7 @@ def word2vec_doc2vec_inception_class():
     cross_val_accuracy(clf, df)
 
 
-# In[9]:
+# In[ ]:
 
 
 doc2vec_inception_class()
